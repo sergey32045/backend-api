cbd7106de38e854fd60de2fc48bc2aa5b4e00e62
roman-morokhovskyi <roman_morokhovskyi@goodyear.com>
2022-05-26 12:32:35 +0300

    fix db-host in ormconfig.js

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   type: 'mysql',
-  host: 'db',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
