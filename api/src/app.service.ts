c3f54fa327e3c19804cab9454cc279e6a775ccb6
roman-morokhovskyi <roman_morokhovskyi@goodyear.com>
2022-05-26 11:44:27 +0300

    fix task-definition.json

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! deploy';
+    return 'Hello World! deploy one more attempt!';
   }
 }
