d263afccab4ba1d53696da1c9f5a5364eb5b2596
double-hope <nadia.prohorchuk@gmail.com>
2023-06-13 00:57:37 +0300

    fix creating tables

--- api/migrations/1685959223860-CreateRoadmapTables.ts
+++ api/migrations/1685959223860-CreateRoadmapTables.ts
@@ -6,9 +6,7 @@ export class CreateRoadmapTables1685959223860 implements MigrationInterface {
             CREATE TABLE IF NOT EXISTS roadmap_category (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255) NOT NULL,
-                status VARCHAR(255) NOT NULL,
-                CHECK (status = "inactive" OR status = "active" OR status = "completed"),
-                last_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                status VARCHAR(50) NOT NULL
             )`);
     await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS roadmap_category_progress (
@@ -16,19 +14,22 @@ export class CreateRoadmapTables1685959223860 implements MigrationInterface {
                 category_id INT UNSIGNED NOT NULL,
                 is_passed TINYINT(1) NOT NULL DEFAULT 0,
                 FOREIGN KEY (user_id) REFERENCES users(id),
-                FOREIGN KEY (category_id) REFERENCES roadmap_category(id)
+                FOREIGN KEY (category_id) REFERENCES roadmap_category(id),
+                last_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )`);
     await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS roadmap_category_tree (
                 parent_id INT UNSIGNED NOT NULL,
                 child_id INT UNSIGNED NOT NULL,
+                PRIMARY KEY (parent_id, child_id),
                 FOREIGN KEY (parent_id) REFERENCES roadmap_category(id),
                 FOREIGN KEY (child_id) REFERENCES roadmap_category(id)
             )`);
     await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS roadmap_category_roles (
-                level_id  INT UNSIGNED NOT NULL,
+                level_id INT UNSIGNED NOT NULL,
                 category_id INT UNSIGNED NOT NULL,
+                PRIMARY KEY (level_id, category_id),
                 FOREIGN KEY (level_id) REFERENCES complexity_levels(id),
                 FOREIGN KEY (category_id) REFERENCES roadmap_category(id)
             )`);
